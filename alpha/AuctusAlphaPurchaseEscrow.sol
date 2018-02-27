f03218a317ffdd34cbc46005031c16ca3ab21fe7
Ariny Guedes <arinyguedes@gmail.com>
2018-02-27 15:48:01 -0300

    Update contracts for testnet deploy

--- alpha/AuctusAlphaPurchaseEscrow.sol
+++ alpha/AuctusAlphaPurchaseEscrow.sol
@@ -7,7 +7,7 @@ library SafeMath {
 		assert(a <= c);
 		return c;
 	}
-	
+
 	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
 		assert(a >= b);
 		return a - b;
@@ -26,44 +26,44 @@ library SafeMath {
 
 
 contract ContractReceiver {
-  function tokenFallback(address from, uint256 value, bytes data) public;
+	function tokenFallback(address from, uint256 value, bytes data) public;
 }
 
 
 contract AuctusAlphaToken {
-  function transfer(address to, uint256 value) public returns (bool);
+	function transfer(address to, uint256 value) public returns (bool);
 }
 
 
 contract AuctusAlphaPurchaseEscrow is ContractReceiver {
 	using SafeMath for uint256;
 
-	address public auctusAlphaToken = 0x0;
+	address public auctusAlphaToken = 0xBf2D5a9cEef4A4B0050323949d54c32Cf4cA0F3d;
 	address public owner;
-	mapping(address => uint256) internal escrowed;
-	
+	mapping(address => uint256) public escrowed;
+
 	event Escrow(address indexed from, uint256 value);
 	event EscrowResult(address indexed from, address indexed to, uint256 value);
-	
+
 	function AuctusAlphaPurchaseEscrow() public {
 		owner = msg.sender;
 	}
-	
+
 	function locked(address from) public constant returns (uint256) {
 		return escrowed[from];
 	}
-	
+
 	function transferOwnership(address newOwner) public {
 		require(msg.sender == owner);
-        owner = newOwner;
-    }
-	
+		owner = newOwner;
+	}
+
 	function tokenFallback(address from, uint256 value, bytes) public {
 		require(msg.sender == auctusAlphaToken);
 		escrowed[from] = escrowed[from].add(value);
 		Escrow(from, value);
 	}
-	
+
 	function escrowResult(address from, address to, uint256 value) public {
 		require(msg.sender == owner);
 		require(escrowed[from] >= value);
--- alpha/AuctusAlphaToken.sol
+++ alpha/AuctusAlphaToken.sol
@@ -7,7 +7,7 @@ library SafeMath {
 		assert(a <= c);
 		return c;
 	}
-	
+
 	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
 		assert(a >= b);
 		return a - b;
@@ -27,7 +27,7 @@ library SafeMath {
 contract ERC20 {
 	uint256 public totalSupply;
 	uint8 public decimals;
-	
+
 	function balanceOf(address who) public constant returns (uint256);
 	function allowance(address owner, address spender) public constant returns (uint256);
 	function transfer(address to, uint256 value) public returns (bool);
@@ -40,45 +40,45 @@ contract ERC20 {
 
 
 contract ERC223 {
-  uint256 public totalSupply;
-  uint8 public decimals;
-  string public name;
-  string public symbol;
-  
-  function balanceOf(address who) public constant returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  function transfer(address to, uint256 value, bytes data) public returns (bool);
-  function transfer(address to, uint256 value, bytes data, string custom_fallback) public returns (bool);
-  
-  event Transfer(address indexed from, address indexed to, uint256 value, bytes indexed data);
+	uint256 public totalSupply;
+	uint8 public decimals;
+	string public name;
+	string public symbol;
+
+	function balanceOf(address who) public constant returns (uint256);
+	function transfer(address to, uint256 value) public returns (bool);
+	function transfer(address to, uint256 value, bytes data) public returns (bool);
+	function transfer(address to, uint256 value, bytes data, string custom_fallback) public returns (bool);
+
+	event Transfer(address indexed from, address indexed to, uint256 value, bytes indexed data);
 }
 
 
 contract ContractReceiver {
-  function tokenFallback(address from, uint256 value, bytes data) public;
+	function tokenFallback(address from, uint256 value, bytes data) public;
 }
 
 
 contract BasicToken is ERC20, ERC223 {
 	using SafeMath for uint256;
 
-	mapping(address => uint256) balances;
-	mapping(address => mapping(address => uint256)) internal allowed;
-	
+	mapping(address => uint256) public balances;
+	mapping(address => mapping(address => uint256)) public allowed;
+
 	function balanceOf(address who) public constant returns (uint256) {
 		return balances[who];
 	}
-  
+
 	function allowance(address owner, address spender) public constant returns (uint256) {
 		return allowed[owner][spender];
 	}
-  
+
 	function approve(address spender, uint256 value) public returns (bool) {
 		allowed[msg.sender][spender] = value;
 		Approval(msg.sender, spender, value);
 		return true;
 	}
-	
+
 	function transferFrom(address from, address to, uint256 value) public returns (bool) {
 		require(value <= allowed[from][msg.sender]);
 		internalTransfer(from, to, value);
@@ -103,7 +103,7 @@ contract BasicToken is ERC20, ERC223 {
 		Approval(msg.sender, spender, allowed[msg.sender][spender]);
 		return true;
 	}
-	
+
 	function transfer(address to, uint256 value) public returns (bool) {
 		internalTransfer(msg.sender, to, value);
 		if (isContract(to)) {
@@ -113,7 +113,7 @@ contract BasicToken is ERC20, ERC223 {
 		Transfer(msg.sender, to, value);
 		return true;
 	}
-	
+
 	function transfer(address to, uint256 value, bytes data) public returns (bool) {
 		internalTransfer(msg.sender, to, value);
 		if (isContract(to)) {
@@ -131,25 +131,25 @@ contract BasicToken is ERC20, ERC223 {
 		Transfer(msg.sender, to, value, data);
 		return true;
 	}
-	
+
 	function isContract(address _address) private constant returns (bool) {
 		uint256 length;
 		assembly {
 			length := extcodesize(_address)
 		}
 		return (length > 0);
-    }
-	
+	}
+
 	function internalTransfer(address from, address to, uint256 value) private {
 		require(value <= balances[from]);
 		balances[from] = balances[from].sub(value);
 		balances[to] = balances[to].add(value);
-    }
-	
+	}
+
 	function callTokenFallback(address to, uint256 value, bytes data) private {
 		ContractReceiver receiver = ContractReceiver(to);
 		receiver.tokenFallback(msg.sender, value, data);
-    }
+	}
 }
 
 
@@ -158,46 +158,43 @@ contract AuctusAlphaToken is BasicToken {
 	uint8 public decimals = 18;
 	string public name = "Auctus Testnet Token";
 	string public symbol = "AUCT";
-	
+
 	address public owner;
-	
-	event Burn(address indexed from, uint256 value);
-	event Mint(address indexed to, uint256 value);
-	
+
 	modifier onlyOwner() {
-        require(msg.sender == owner);
+		require(msg.sender == owner);
 		_;
-    }
-	
+	}
+
 	function AuctusAlphaToken() public {
 		owner = msg.sender;
 		totalSupply = 0;
 	}
-	
+
 	function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-	
-	function mint(address to, uint256 value) onlyOwner public  {
+		owner = newOwner;
+	}
+
+	function mint(address to, uint256 value) onlyOwner public {
 		balances[to] = balances[to].add(value);
 		totalSupply = totalSupply.add(value);
-		Mint(to, value);
+		Transfer(0x0, to, value);
 	}
-	
+
 	function burn(uint256 value) public returns (bool) {
 		internalBurn(msg.sender, value);
-        Burn(msg.sender, value);
+		Transfer(msg.sender, 0x0, value);
 		return true;
-    }
+	}
 
-    function burnFrom(address from, uint256 value) public returns (bool) {
+	function burnFrom(address from, uint256 value) public returns (bool) {
 		require(value <= allowed[from][msg.sender]);
 		internalBurn(from, value);
 		allowed[from][msg.sender] = allowed[from][msg.sender].sub(value);
-		Burn(from, value);
+		Transfer(from, 0x0, value);
 		return true;
-    }
-	
+	}
+
 	function internalBurn(address from, uint256 value) private {
 		require(value <= balances[from]);
 		balances[from] = balances[from].sub(value);
